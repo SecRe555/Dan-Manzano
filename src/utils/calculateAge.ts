5e648ed32f5237c0971524cbe6b7d681dbfdf5db
Daniel Manzano <chideee.hardy@gmail.com>
2025-05-01 21:11:40 -0600

    Estructura de experencia realizada

---
+++ src/utils/calculateAge.ts
@@ -0,0 +1,13 @@
+export const calculateAge = () => {
+  const birthDate = new Date("2001-10-02");
+  const today = new Date();
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    return age - 1;
+  }
+  return age;
+};
